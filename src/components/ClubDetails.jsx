dfcd6179da11942b037263ccb79c6282edb84955
asif <asif69284@gmail.com>
2024-07-25 16:25:32 +0545

    commit

---
+++ src/components/ClubDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FootballClubDetails from "/public/FootBallClubDetails.json"
+
+export default function ClubDetails() {
+  return (
+    <div>
+      {FootballClubDetails}
+    </div>
+  )
+}
---
+++ src/pages/FootballTable.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FootballClubDetails from "/public/FootBallClubDetails.json"
+
+export default function FootballTable() {
+  return (
+    <div>
+      {FootballClubDetails}
+    </div>
+  )
+}
