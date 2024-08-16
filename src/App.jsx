f4833e499fdb258f6a8d78438dcaa47f1c8d7837
asif <asif69284@gmail.com>
2024-08-16 17:59:56 +0545

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import FootballTable from "./pages/footballTable";
+import FootballTable from "./pages/FootballTable";
 import ClubDetails from "./components/ClubDetails";
 
 export default function App() {
